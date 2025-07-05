00ce43c6866297f95e10a9e2d39a1efaabf71fc5
= <=>
2025-07-05 11:08:32 -0700

    Trying Playing with Native Again

---
+++ app/index.tsx
@@ -0,0 +1,20 @@
+import { offers } from "@/constants";
+import { FlatList, Pressable, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import "./globals.css";
+export default function Index() {
+  return (
+    <SafeAreaView>
+      <FlatList data= {offers} renderItem= {({item,index})=>{
+        
+        return( <View>
+          <Pressable className = "bg-amber-600 my-3 h-48 rounded-xl">
+            <Text >{item.title}</Text>
+          </Pressable>
+        </View>
+        
+  )}
+      }/>
+    </SafeAreaView>
+  );
+}
