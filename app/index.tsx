406e8756cea3dc16d7e5ff6413081b45ec90c342
= <=>
2025-07-05 13:23:22 -0700

    Index Component

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,55 @@
-import { offers } from "@/constants";
-import { FlatList, Pressable, Text, View } from "react-native";
+import CartButton from "@/components/CartButton";
+import { images, offers } from "@/constants";
+import cn from 'clsx';
+import { FlatList, Image, Pressable, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import "./globals.css";
 export default function Index() {
   return (
-    <SafeAreaView>
-      <FlatList data= {offers} renderItem= {({item,index})=>{
-        
+    <SafeAreaView className="flex-1 bg-white"> 
+      
+      <FlatList data= {offers} 
+      
+      renderItem={({item,index})=>{
+        const isEven: boolean = index%2 === 0;
         return( <View>
-          <Pressable className = "bg-amber-600 my-3 h-48 rounded-xl">
-            <Text >{item.title}</Text>
+          <Pressable className = {cn("offer-card", isEven?'flex-row-reverse':'flex-row')} style={{backgroundColor: `${item.color}`}}
+          android_ripple={{color:"#ffff22"}}
+          >
+                    {({pressed})=>(
+                      <>
+                      <View className="h-full w-1/2">
+                      <Image source= {item.image} className= "size-full" resizeMode="contain"></Image>
+                      </View>
+                      <View className={cn("offer-card__info",isEven?'pl-10':'pr-10')}>
+                        <Text className="h1-bold text-white leading-tight">{item.title}</Text>
+                        <Image source={images.arrowRight} resizeMode="contain" className="size-10" tintColor='#ffffff'></Image>
+                      </View>
+                      </>
+                    )}
+
           </Pressable>
         </View>
-        
   )}
-      }/>
+      
+
+      }
+      
+      contentContainerClassName="pb-28 px-5"
+      ListHeaderComponent={()=>(
+        <View className="flex-between flex-row w-full my-5 px-5">
+        <View className="flex-start">
+          <Text className="small-bold text-primary">DELIVER TO</Text>
+          <TouchableOpacity className="flex-center flex-row gap-x-2 mt-0.5">
+          <Text className="text-dark-100 paragraph-bold ">Cairo</Text>
+          <Image source={images.arrowDown} className="size-3" resizeMode="contain"></Image>
+          </TouchableOpacity>
+
+        </View>
+        <CartButton/>
+      </View>
+      )}
+      />
     </SafeAreaView>
   );
 }
